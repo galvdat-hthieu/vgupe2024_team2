88b5043c3a8786502269e732e4e3c605d1204a1c
Do Minh Quang <quangdm961@gmail.com>
2024-03-15 21:54:58 +0700

    update search book function

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from home.views import *
 app_name = 'home'
 urlpatterns = [
     path('', index, name='index'),
-    path('gallery', galleryView.as_view(), name="gallery")
+    path('gallery', galleryView.as_view(), name="gallery"),
+    path('search', search, name = "search"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,10 +2,38 @@ from django.shortcuts import render, HttpResponse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
+from django.db.models import Q
+
 
 # Create your views here.
 def index(request):
   return render(request, 'home/index.html')
+
+def search(request):
+
+  books = Book.objects.all()
+  cateList = ['liteCate','socieCate','naturCate','techCate','poliCate','romanCate','enterCate','otherCate']
+
+  
+
+  if request.GET.get("book_search"):
+    keyword = request.GET.get("book_search")
+    books = Book.objects.filter(title__contains = keyword)
+    
+
+  if request.GET.getlist("category"):
+    selected_categories = request.GET.getlist('category')
+    filter_condition = Q()
+    for category in selected_categories:
+      filter_condition &= Q(**{category: True})
+    books = books.filter(filter_condition)
+      
+
+  context = {
+    'books':books
+  }
+
+  return render(request, 'home/gallery.html',context)
 
 class galleryView(LoginRequiredMixin, View):
   login_url = "/user/login"
