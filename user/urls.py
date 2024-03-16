868732bc088366d026f13cc30a70d9233b960389
Miki <tamnhu03@gmail.com>
2024-03-16 22:06:11 +0700

    update mod page

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,5 @@ from user.views import *
 app_name = 'user'
 urlpatterns = [
     path('login/',loginView.as_view(), name='login'),
-    path("info/", infoView.as_view(), name="info") 
+    path("info/", infoView.as_view(), name="info")
 ]
