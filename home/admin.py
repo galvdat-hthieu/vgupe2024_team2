204fc9475498400feeb718b428fc0e64008e95e7
TheFabulousP <anphu.hlap@gmail.com>
2024-03-27 11:56:53 +0700

    register reviewAdmin

--- home/admin.py
+++ home/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Book, User, Review, Borrowance, Copy
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('bookID', 'userID', 'rating', 'created_at')
 
 admin.site.register(Book)
 admin.site.register(User)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(Borrowance)
 admin.site.register(Copy)
